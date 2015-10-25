aa6b280898935fd6eaf05d7b369c2595dbb61d9a
davidbebb <davidbebb@gmail.com>
2015-10-25 21:31:05 +0000

    added protractor tests

--- test/e2e/toDospec.js
+++ test/e2e/toDospec.js
@@ -1,17 +1,20 @@
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-    browser.get('http://localhost:8080');
+describe('todo list', function() {
 
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
     element(by.model('listCtrl.toDoText')).sendKeys('write first protractor test');
     element(by.css('[ng-click="listCtrl.addToDo()"]')).click();
+  });
 
+  it('should add a todo', function() {
     var todoList = element.all(by.repeater('item in listCtrl.toDoList.items'));
     expect(todoList.count()).toEqual(1);
-    // expect(todoList.get(2).getText()).toEqual('write first protractor test');
+  });
 
-    // You wrote your first test, cross it off the list
-    todoList.get(0).element(by.css('input')).click();
-    var completedAmount = element.all(by.css('.done-true'));
-    expect(completedAmount.count()).toEqual(2);
+  it('should mark a done item as done', function() {
+    var todoList = element.all(by.repeater('item in listCtrl.toDoList.items'));
+    todoList.get(0).element(by.model('item.done')).click();
+    var completedAmount = element.all(by.css('.toDo-true'));
+    expect(completedAmount.count()).toEqual(1);
   });
 });
