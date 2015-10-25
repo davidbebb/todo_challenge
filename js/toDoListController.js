f098d7650585a77c38816c7feaebf31e45421c35
davidbebb <davidbebb@gmail.com>
2015-10-25 16:27:14 +0000

    now adds to todo list, no persistence

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -1,13 +1,12 @@
 toDoList.controller('ToDoListController', [function() {
   var self = this;
 
-  self.addToDo = function() {
-    self.toDoList = {
-      items:
-        [
-          {text: 'Test todo'},
-      ],};
+  self.toDoList = [ ];
+  self.toDoList.items = [];
 
+  self.addToDo = function() {
+    self.toDoList.items.push({text: self.toDoText, done: false});
+    self.toDoText = '';
   };
 
 },]);
