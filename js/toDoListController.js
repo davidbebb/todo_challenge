2fa1704e4e433da455d8892adf2df3398142c168
davidbebb <davidbebb@gmail.com>
2015-10-25 18:21:00 +0000

    added edit, delete, item counting and done marking

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -9,8 +9,40 @@ toDoList.controller('ToDoListController', [function() {
     self.toDoText = '';
   };
 
-  self.markDone = function() {
-    e;
+  self.markDone = function(index) {
+    self.toDoList.items[index].done = true;
+  };
+
+  self.editItem = function(index) {
+    self.toDoText = self.toDoList.items[index].text;
+    self.deleteItem(index);
+  };
+
+  self.deleteItem = function(index) {
+    self.toDoList.items.splice(index, 1);
+  };
+
+  self.countItems = function() {
+    return self.toDoList.items.length;
+  };
+
+  self.countItemsToDo = function() {
+    var numberToDo = 0;
+    for (var i = 0; i < self.toDoList.items.length; i++) {
+      if (self.toDoList.items[i].done === false) {
+        numberToDo += 1;
+      };
+    };
+
+    return numberToDo;
+  };
+
+  self.deleteDone = function() {
+    for (var i = 0; i < self.toDoList.items.length; i++) {
+      if (self.toDoList.items[i].done === true) {
+        self.deleteItem(i);
+      };
+    };
   };
 
 },]);
--- test/toDo.spec.js
+++ test/toDo.spec.js
@@ -7,7 +7,7 @@ describe('ToDo', function() {
     ctrl = $controller('ToDoListController');
   }));
 
-  it('initialises with a to do list', function() {
+  it('initialises with a blank text box', function() {
     expect(ctrl.toDoText).toBeUndefined();
   });
 
@@ -21,6 +21,52 @@ describe('ToDo', function() {
     ctrl.toDoText = 'Test todo';
     ctrl.addToDo();
     expect(ctrl.toDoList.items[0].done).toBe(false);
+  });
+
+  it('allows a task to be deleted', function() {
+    ctrl.toDoText = 'Test todo';
+    ctrl.addToDo();
+    ctrl.deleteItem(0);
+    expect(ctrl.toDoList.items[0]).toBeUndefined();
+  });
+
+  it('allows an element to be edited', function() {
+    ctrl.toDoText = 'Test todo';
+    ctrl.addToDo();
+    ctrl.editItem(0);
+    expect(ctrl.toDoText).toEqual('Test todo');
+  });
+
+  it('allows an element to be marked done', function() {
+    ctrl.toDoText = 'Test todo';
+    ctrl.addToDo();
+    ctrl.markDone(0);
+    expect(ctrl.toDoList.items[0].done).toBe(true);
+  });
+
+  it('counts items in list', function() {
+    ctrl.toDoText = 'Test todo';
+    ctrl.addToDo();
+    expect(ctrl.countItems()).toEqual(1);
+  });
+
+  it('counts items to do', function() {
+    ctrl.toDoText = 'Test todo';
+    ctrl.addToDo();
+    ctrl.toDoText = 'Test todo 2';
+    ctrl.addToDo();
+    ctrl.markDone(0);
+    expect(ctrl.countItemsToDo()).toEqual(1);
+  });
+
+  it('deletes items that are done', function() {
+    ctrl.toDoText = 'Test todo';
+    ctrl.addToDo();
+    ctrl.toDoText = 'Test todo 2';
+    ctrl.addToDo();
+    ctrl.markDone(0);
+    ctrl.deleteDone();
+    expect(ctrl.countItemsToDo()).toEqual(ctrl.countItems());
   });
 
 });
